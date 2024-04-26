157e65f162a2f9601b0513bc729d5613d9fdbe11
veekshith <veekshithm@gmail.com>
2024-04-26 16:55:33 +0530

    changed backend url

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -1,4 +1,4 @@
 # Store workspace public IP to config file
-IP_VAR="https://qkart-backend-json-server.onrender.com"
+IP_VAR="https://qkart.backend.projects.api.veekshith.dev"
 CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
 echo $CONFIG > src/ipConfig.json
